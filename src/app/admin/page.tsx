7003c8203e8c78835ab62d9e44541a4ad5aee70f
azaaa-9 <az95342030@gmail.com>
2025-01-16 18:03:09 +0800

    front-end

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Header } from "./_components/header";
+ 
+type CategoryType = {
+  categoryName: string;
+  _id: string;
+}
+export default function Home() {
+ 
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+ 
+ 
+  const addCategory = async () => {  
+    
+    const categoryName = prompt('Enter new category name');
+    const response = await fetch('http://localhost:5000/food-category/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ categoryName }),
+    });
+    const data = await response.json();
+    setCategories([...categories, data.newItem]);
+}
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:5000/food-category');
+      const data = await response.json();
+      setCategories(data);
+    };
+ 
+    fetchData();
+  })
+  return (
+    <div>
+        <Header />
+      {categories.map((category) => (
+        <div key={category._id}> {category.categoryName}
+        </div>
+      ))}
+ 
+      <button className="bg-green-400 p-3 mt-2 rounded-lg" onClick={addCategory}> ADD NEW </button>
+    </div>
+  );
+}
+ 
+ 
