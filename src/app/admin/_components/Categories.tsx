b02fcbbe203606b661d59287034655fb2ff1d8f6
azaaa-9 <az95342030@gmail.com>
2025-01-20 17:05:23 +0800

    HOME PAGE ehelsen

--- src/app/admin/_components/Categories.tsx
+++ src/app/admin/_components/Categories.tsx
@@ -1,74 +1,3 @@
-// "use client";
-// import { useEffect, useState } from "react";
-// import { Badge } from "@/components/ui/badge";
-// import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger,} from "@/components/ui/dialog"
- 
-// type CategoryType = {
-//   categoryName: string;
-//   _id: string;
-// }
-// export function Categories() {
- 
-//   const [categories, setCategories] = useState<CategoryType[]>([]);
- 
- 
-//   const addCategory = async () => {  
-    
-//     const categoryName = prompt('Enter new category name');
-//     const response = await fetch('http://localhost:5000/food-category/', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ categoryName }),
-//     });
-//     const data = await response.json();
-//     setCategories([...categories, data.newItem]);
-// }
- 
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const response = await fetch('http://localhost:5000/food-category');
-//       const data = await response.json();
-//       setCategories(data);
-//     };
- 
-//     fetchData();
-//   })
-
-//   return (
-//     <div className=" w-full p-6 rounded-2xl  flex flex-col gap-4 bg-background ">
-//       <h4 className=" text-xl font-semibold  ">Dishes Category</h4>
-//       <div className="flex flex-wrap gap-3 cursor-pointer">
-//         {categories?.map((category) => {
-//           return (
-//             <Badge
-//               key={category._id}
-//               className=" rounded-full border py-2 px-4 flex gap-2 text-sm font-medium text-black bg-white"
-//             >
-//               {category.categoryName}
-//             </Badge>
-          
-//           );
-          
-//         })} <Dialog>
-// <DialogTrigger><button className="bg-red-500 p-2 mt-2 rounded-full" onClick={addCategory}> + </button></DialogTrigger>
-// <DialogContent>
-//   <DialogHeader>
-//     <DialogTitle>Are you absolutely sure?</DialogTitle>
-//     <DialogDescription>
-//       This action cannot be undone. This will permanently delete your account
-//       and remove your data from our servers.
-//     </DialogDescription>
-//   </DialogHeader>
-// </DialogContent>
-// </Dialog>
- 
-//       </div>
-//     </div>
-    
-//   );
-// };
 
 "use client";
 import { useEffect, useState } from "react";
@@ -97,7 +26,7 @@ export function Categories() {
 
   const addCategory = async () => {
 
-    const response = await fetch("http://localhost:6000/food-category/", {
+    const response = await fetch("http://localhost:5000/food-category/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -113,7 +42,7 @@ export function Categories() {
  
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:6000/food-category");
+      const response = await fetch("http://localhost:5000/food-category");
       const data = await response.json();
       setCategories(data);
     };
@@ -148,7 +77,7 @@ export function Categories() {
             </DialogHeader>
             <div className="flex flex-col gap-4">
               <Input
-                placeholder="Категорийн нэр оруулна уу"
+                placeholder="Add Category"
                 value={newCategoryName}
                 onChange={(e) => setNewCategoryName(e.target.value)}
               />
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,50 +1,7 @@
-"use client";
-import { useEffect, useState } from "react";
- 
-type CategoryType = {
-  categoryName: string;
-  _id: string;
-}
-export default function Home() {
- 
-  const [categories, setCategories] = useState<CategoryType[]>([]);
- 
- 
-  const addCategory = async () => {  
-    
-    const categoryName = prompt('Enter new category name');
-    const response = await fetch('http://localhost:6000/food-category/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ categoryName }),
-    });
-    const data = await response.json();
-    setCategories([...categories, data.newItem]);
-}
- 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:6000/food-category');
-      const data = await response.json();
-      setCategories(data);
-    };
- 
-    fetchData();
-  })
-  return (
-  
-    <div>
-      {categories.map((category) => (
-        <div key={category._id}> {category.categoryName}
-        </div>
-      ))}
- 
-      <button className="bg-green-400 p-3 mt-2 rounded-lg" onClick={addCategory}> Add new</button>
-    </div>
+import { Menu } from "./menu/Menu"
 
-  );
+export function Home() {
+  return(
+    <div><Menu  /></div>
+  )
 }
- 
- 
