41aa6d45190972ea3120db0cd04781f86cd4c7d1
azaaa-9 <az95342030@gmail.com>
2025-01-29 17:12:30 +0800

    localhost:3000 sheet

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -2,37 +2,68 @@ import { ShoppingCart } from "lucide-react";
 import { Facebook } from "lucide-react";
 import { Instagram } from "lucide-react";
 import { User } from "lucide-react";
-import "./styles.css"
+import "./styles.css";
 import { Categories } from "../admin/_components/Categories";
 import { Footer } from "../admin/_components/footer";
+import {
+  Sheet,
+  SheetTrigger,
+  SheetHeader,
+  SheetContent,
+  SheetTitle,
+  SheetDescription,
+} from "@/components/ui/sheet";
 export default function App() {
   return (
     <div>
       <div className="w-[full] h-[68px] bg-[#18181B] flex items-center justify-between  ">
         <img className="w-[146px] h-[44px] ml-16 " src="Logo.png" />
+
         <div className=" flex  mr-16 gap-4">
-          <ShoppingCart className="text-black bg-white rounded-full  "/>
-          <User className="text-black bg-[#EF4444] rounded-full"/>
+          <Sheet>
+            <SheetTrigger>
+              <ShoppingCart className="text-black bg-white rounded-full  " />
+            </SheetTrigger>
+            <SheetContent className="bg-[#404040]">
+              <SheetHeader>
+                <SheetTitle className="flex gap-3 text-white">
+                  <ShoppingCart />
+                  Order detail
+                </SheetTitle>
+                <SheetDescription className="text-2xl text-black border border-solid rounded-full flex bg-white justify-center">
+                  Cart Order
+                </SheetDescription>
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
+
+          <User className="text-black bg-[#EF4444] rounded-full" />
         </div>
       </div>
       <img className="w-full h-[500]" src="BG.png" />
       <div className="text-[30px] font-[600] text-black ml-16 w-full justify-center ">
         Categories
         <div className="text-xl pt-10 flex gap-8 text-center ">
-          <p className="bg-red-500 border border-solid-1px rounded-full p-2">Appetizers</p>
+          <p className="bg-red-500 border border-solid-1px rounded-full p-2">
+            Appetizers
+          </p>
           <p className="border rounded-full border-solid-1px p-2">Salads</p>
           <p className="border rounded-full border-solid-1px p-2">Pizzas</p>
           <p className="border rounded-full border-solid-1px p-2">Side dish</p>
           <p className="border rounded-full border-solid-1px p-2">Brunch</p>
-          <p className="border rounded-full border-solid-1px p-2">Luch favorites</p>
+          <p className="border rounded-full border-solid-1px p-2">
+            Luch favorites
+          </p>
           <p className="border rounded-full border-solid-1px p-2">Desserts</p>
-          <p className="border rounded-full border-solid-1px p-2">Main Dishes</p>
-          <p className="border rounded-full border-solid-1px p-2">Fish & Sea foods</p>
+          <p className="border rounded-full border-solid-1px p-2">
+            Main Dishes
+          </p>
+          <p className="border rounded-full border-solid-1px p-2">
+            Fish & Sea foods
+          </p>
           <p className="border rounded-full border-solid-1px p-2">Beverages</p>
-
         </div>
       </div>
-
 
       <div className="bg-white w-[400px] h-[350px] rounded-3xl shadow-lg">
         <img
@@ -54,7 +85,3 @@ export default function App() {
     </div>
   );
 }
- 
- 
- 
- 
